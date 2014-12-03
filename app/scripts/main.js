2db89805796c56dab9c1da51f1ae01fb74698b63
Daniel Stern <daniel@danielstern.ca>
2014-12-02 23:03:15 -0500

    added awesome interface implementing material design

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,13 @@
 var game = new Squaretide();
 
 game.startGame({
-	time:60
+    time: 60
 });
+
+var showInstructions = false;
+
+var instructions = document.getElementById("instructionsButton");
+instructions.addEventListener("click", function() {
+	showInstructions = !showInstructions;
+	document.getElementById('game').setAttribute("show-instructions",showInstructions);
+})
