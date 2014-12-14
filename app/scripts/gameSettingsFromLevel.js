0a776e1b7d5e90e050e03f25c621b512d030118b
Daniel Stern <daniel@danielstern.ca>
2014-12-14 16:08:17 -0500

    tweaked difficulties

--- app/scripts/gameSettingsFromLevel.js
+++ app/scripts/gameSettingsFromLevel.js
@@ -6,17 +6,23 @@ function gameSettingsFromLevel(level){
 		return Math.floor(level / 5);
 	}
 
+	function everyFourth(level) {
+		return Math.floor(level / 4);
+	}
+
+	function everySeventh(level) {
+		return Math.floor(level / 7);
+	}
+
 	function powerOf(level) {
 		return Math.pow(level,9/8);
 	}
 
-
-
-	var colors = 4 + everyThird(level); // more colors is more hard	
-	var chainLength = 3 + everyFifth(level); // more combo is more hard	
+	var colors = 4 + everyFourth(level); // more colors is more hard	
+	var chainLength = 3 + everySeventh(level); // more combo is more hard	
 	var targetScore = powerOf(level) * 10000; // higher target score is more hard
 	var scorePerTile = 100 + 10 * powerOf(level); // more score per tile is more awesome
-	var duration = 25 + level / 2; // shorter duration is more hard
+	var duration = 20 + level * 5; // shorter duration is more hard
 	// var duration = 60 - level * 2; // shorter duration is more hard
 	var rows = 4 + everyThird(level);
 	var columns = 4 + everyThird(level);
